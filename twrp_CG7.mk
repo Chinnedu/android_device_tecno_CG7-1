6855a0fe318227c3ed1c617c43bd2d9aa6eab8bd
infinixs5lite <kelvinchinedu97@gmail.com>
2023-02-08 07:07:26 +0100

    Update twrp_CG7.mk

--- twrp_CG7.mk
+++ twrp_CG7.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from CG7 device
-$(call inherit-product, device/Tecno/CG7/device.mk)
+$(call inherit-product, device/tecno/CG7/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
